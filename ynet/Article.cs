c4fb7123af63b43a6d0c0a6e3d7d38fc05e8acc7
sl4vkek <43294802+sl4vkek@users.noreply.github.com>
2019-05-14 17:28:29 +0300

    Add files via upload

---
+++ ynet/Article.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Collections.Generic;
+using System.Web.Script.Serialization;
+
+namespace ynet
+{
+    public class Article
+    {
+        public string Article_url;
+        public string Article_id;
+        public string Article_uri;
+        public Article(string article_url)
+        {
+            Uri parsed_url = new Uri(article_url);
+            this.Article_id = Path.GetFileName(parsed_url.AbsolutePath).Split(',')[2];
+            this.Article_uri = parsed_url.Scheme + "://" + parsed_url.Host + "/";
+        }
+
+        public Comment[] GetComments()
+        {
+            List<Comment> comments = new List<Comment>();
+
+            string raw_json = new WebClient().DownloadString(this.Article_uri + 
+                "Ext/Comp/ArticleLayout/Proc/ShowTalkBacksAjax/v2/0,12990," + 
+                this.Article_id + "-desc-68-0-1,00.html");
+            JavaScriptSerializer jsonSerializer = new JavaScriptSerializer();
+            dynamic comment_json = jsonSerializer.Deserialize<dynamic>(raw_json);
+            foreach (dynamic comment_obj in comment_json["rows"])
+            {
+                comments.Add(
+                    new Comment(
+                        this,
+                        comment_obj["name"],
+                        string.Empty,
+                        comment_obj["location"],
+                        comment_obj["title"],
+                        comment_obj["text"],
+                        comment_obj["ts"],
+                        comment_obj["tc"],
+                        comment_obj["id"],
+                        comment_obj["parent_id"]
+                    ).SetToRetrievedCommentFormat());
+            }
+            return comments.ToArray();
+        }
+
+        public Comment GetCommentByCommentNum(int commentNum)
+        {
+            Comment[] all_comments = this.GetComments();
+            foreach(Comment comment in all_comments)
+            {
+                if (comment.CommentNum == commentNum)
+                {
+                    return comment;
+                }
+            }
+            return null;
+        }
+
+        public Comment[] GetCommentsByWriter(string WriterName)
+        {
+            List<Comment> commentsByWriter = new List<Comment>();
+
+            Comment[] all_comments = this.GetComments();
+            foreach(Comment comment in all_comments)
+            {
+                if (comment.Name == WriterName)
+                {
+                    commentsByWriter.Add(comment);
+                }
+            }
+
+            return commentsByWriter.ToArray();
+        }
+
+        public bool HasCommentsByWriter(string WriterName)
+        {
+            Comment[] commentsByWriter = this.GetCommentsByWriter(WriterName);
+            if (commentsByWriter == null || commentsByWriter.Length == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ ynet/Comment.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ynet
+{
+    public class Comment
+    {
+        public Article _Article;
+        public string Name;
+        public string Email;
+        public string Location;
+        public string Title;
+        public string Text;
+        public int Likes;
+        public int CommentNum;
+        public DateTime CommentDate;
+        public int CommentId;
+        public int ParentId;
+        public bool isReply;
+
+        public Comment(Article _article,
+            string Name,
+            string Email,
+            string Location,
+            string Title,
+            string Text,
+            int Likes,
+            int CommentNum,
+            int CommentId,
+            int ParentId)
+        {
+            this.Name = Name;
+            this.Email = Email;
+            this.Location = Location;
+            this.Title = Title;
+            this.Text = Text;
+            this.Likes = Likes;
+            this.CommentNum = CommentNum;
+            this.CommentId = CommentId;
+            this.ParentId = ParentId;
+            if (ParentId == 0)
+            {
+                this.isReply = false;
+            } else
+            {
+                this.isReply = true;
+            }
+        }
+
+        public Comment[] GetReplies()
+        {
+            List<Comment> replies = new List<Comment>();
+
+            Comment[] all_comments = this._Article.GetComments();
+            foreach (Comment comment in all_comments)
+            {
+                if (comment.ParentId == this.CommentId)
+                {
+                    replies.Add(comment);
+                }
+            }
+
+            return replies.ToArray();
+        }
+
+        public Comment SetToRetrievedCommentFormat()
+        {
+            this.Name = this.Name.Replace("&quot;", "\"").Replace("&#39;", "'");
+            this.Title = this.Title.Replace("&quot;", "\"").Replace("&#39;", "'");
+            this.Text = this.Text.Replace("&quot;", "\"").Replace("&#39;", "'").Replace("<br/>", "\n");
+            return this;
+        }
+
+        public Comment GetParentComment()
+        {
+            Comment[] all_comments = this._Article.GetComments();
+            foreach(Comment comment in all_comments)
+            {
+                if (this.ParentId == comment.CommentId)
+                {
+                    return comment;
+                }
+            }
+            return null;
+        }
+
+        public string Reply(Comment comment)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                string post_url = this._Article.Article_uri +
+                    "YediothPortal/Ext/TalkBack/CdaTalkBackTrans/0,2499," + 
+                    this._Article.Article_id +
+                    "-0-68-13108-0---" + 
+                    this.CommentId.ToString() + 
+                    ",00";
+                var reqparm = new System.Collections.Specialized.NameValueCollection();
+                reqparm.Add("WSGBRWSR", "FF");
+                reqparm.Add("name", comment.Name);
+                reqparm.Add("email", comment.Email);
+                reqparm.Add("Location", comment.Location);
+                reqparm.Add("title", comment.Title);
+                reqparm.Add("description", comment.Text);
+                byte[] responsebytes = wc.UploadValues(post_url, "POST", reqparm);
+                return Encoding.UTF8.GetString(responsebytes);
+            }
+        }
+
+        public string Post()
+        {
+            using (WebClient wc = new WebClient())
+            {
+                string post_url = this._Article.Article_uri +
+                    "YediothPortal/Ext/TalkBack/CdaTalkBackTrans/0,2499," +
+                    this._Article.Article_id +
+                    "-0-68-546-0---0,00.html";
+                var reqparm = new System.Collections.Specialized.NameValueCollection();
+                reqparm.Add("WSGBRWSR", "FF");
+                reqparm.Add("name", this.Name);
+                reqparm.Add("email", this.Email);
+                reqparm.Add("Location", this.Location);
+                reqparm.Add("title", this.Title);
+                reqparm.Add("description", this.Text);
+                byte[] responsebytes = wc.UploadValues(post_url, "POST", reqparm);
+                return Encoding.UTF8.GetString(responsebytes);
+            }
+        }
+    }
+}
